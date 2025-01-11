d2f10d714478efa90c90fb73c6bd96eff277136b
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-01-11 11:49:55 -0600

    Complete 05_sumAll
    Passes two arguments and sums every number in between, including the
    parameter numbers.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let result = 0;
+    if(num1 < 0 || typeof(num1) !== "number" || !Number.isInteger(num1)){
+        return 'ERROR';
+    }
+    else if(num2 < 0 || typeof(num2) !== "number" || !Number.isInteger(num1)){
+        return 'ERROR';
+    }
+    else{
+        if (num2 > num1){
+            for(i = num2; i >=0; i--){
+                result += i;
+        }
+        return result;
+        }
+        else{
+            for(i = num1; i >=num2; i--){
+                result += i;
+            }
+            return result;
+        }
+    }
 };
+
+sumAll(1, 4); // returns the sum of 1 + 2 + 3 + 4 which is 10
 
 // Do not edit below this line
 module.exports = sumAll;
