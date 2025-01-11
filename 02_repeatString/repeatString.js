3ce2053a3862fe807c7cb9b9c72a47f38af8700b
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-01-10 19:06:29 -0600

    Complete exercise 02_repeatString
    Exercise repeats a string multiple times. Passed all tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    let result = "";
+    for (let i = 0; i < num; i++) {
+        result += string;
+    }
+    
+    return result;
+}
 
-};
+repeatString('hey', 3); // returns 'heyheyhey'
 
 // Do not edit below this line
 module.exports = repeatString;
