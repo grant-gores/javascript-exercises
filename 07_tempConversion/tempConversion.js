c12e18075351d62f118741fef04a7ca0af91d942
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-01-11 14:24:35 -0600

    Completed exercise 07_tempConversion
    Code has 2 functions, one to convert Celsius to fahrenheit and
    vice versa.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = 5/9*(fahrenheit-32);
+  let result = Math.round(celsius * 10)/10
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9/5) + 32;
+  let result = Math.round(fahrenheit * 10)/10
+  return result;
 };
+
+convertToCelsius(32) // fahrenheit to celsius, should return 0
+
+convertToFahrenheit(0) // celsius to fahrenheit, should return 32
 
 // Do not edit below this line
 module.exports = {
