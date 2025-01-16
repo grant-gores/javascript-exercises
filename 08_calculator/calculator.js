de5402fb29052b73d7c485edcb52aead4741bd2c
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-01-16 12:25:27 -0600

    Complete first three steps of 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,15 @@
-const add = function() {
-	
+const add = function(a, b) {
+	result = a + b;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  result = a - b;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  array.reduce((total, current) => total + current, 0);
 };
 
 const multiply = function() {
