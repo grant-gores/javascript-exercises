236f3bd2425c77a21513b405824a8309e64dbfea
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-01-17 14:20:03 -0600

    Complete javascript exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear(); // Get current year
 
+    return people
+        .map(person => ({
+            ...person,
+            age: (person.yearOfDeath || currentYear) - person.yearOfBirth
+        }))
+        .sort((a, b) => b.age - a.age)[0]; // Sort descending & return oldest
 };
 
 // Do not edit below this line
