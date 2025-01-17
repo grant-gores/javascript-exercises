4d1cfe06c64907f6f75ae3943004395f982ec7a6
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-01-17 08:55:14 -0600

    Complete javascript exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr = str.split("").filter(char => /[a-zA-Z0-9]/.test(char));
+    let forArr = arr.join("").toLowerCase();
+    let revArr = arr.slice().reverse().join("").toLowerCase();
+    console.log(forArr);
+    console.log(revArr);
+    if (forArr === revArr){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
+
+// Example usage
+console.log(palindromes("racecar, racecar")); // Output: "racecarracecar"
 
 // Do not edit below this line
 module.exports = palindromes;
